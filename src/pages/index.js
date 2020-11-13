61829857627a336678f9f13d5245f165c292ad17
jhamPac <cloud@kevia.me>
2020-11-12 20:39:38 -0700

    Add screen recording code and reverse engineer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import * as React from 'react'
+import useSR from '../mediarecorder/usescreenrecording'
 
 export default function App() {
+    const { isRecording, recording, toggleRecording } = useSR()
+    console.log(isRecording)
     return <div>Telebugg</div>
 }

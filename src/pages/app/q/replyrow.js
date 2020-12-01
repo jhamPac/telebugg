c0ae557b569f769bc113c829df8a3d3178e432d6
jhamPac <cloud@kevia.me>
2020-11-30 21:31:10 -0700

    Save point after creating an Avatar component

--- src/pages/app/q/replyrow.js
+++ src/pages/app/q/replyrow.js
@@ -4,7 +4,17 @@ import styled from 'styled-components'
 export default function ReplyRow(props) {
     return (
         <Row>
-            <h2>Hello</h2>
+            <p>
+                As mentioned in the accepted answer, this error is usually caused when the
+                API returns data in a format, say object, instead of in an array. If no
+                existing answer here cuts it for you, you might want to convert the data
+                you are dealing with into an array with something like:
+            </p>
+            <UserBlock>
+                <Avatar>
+                    <img src="https://i.pravatar.cc/64?img=3" />
+                </Avatar>
+            </UserBlock>
         </Row>
     )
 }
@@ -12,6 +22,17 @@ export default function ReplyRow(props) {
 const Row = styled.div`
     background: blue;
     width: 100%;
-    height: 128px;
     margin-bottom: 32px;
+`
+
+const UserBlock = styled.div`
+    background: white;
+    width: 100%;
+    height: 64px;
+`
+
+const Avatar = styled.div`
+    width: 64px;
+    height: 64px;
+    background: black;
 `

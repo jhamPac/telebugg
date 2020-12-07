085382024aa6ac63d625f99fca6cc97b96f6e7cf
jhamPac <cloud@kevia.me>
2020-12-07 00:14:04 -0700

    Add plugin in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         `gatsby-transformer-json`,
+        `gatsby-plugin-react-helmet`,
         {
             resolve: `gatsby-source-filesystem`,
             options: {

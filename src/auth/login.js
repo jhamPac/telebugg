40594bf6164fceca6d899015b9ed9c146378b0f4
jhamPac <cloud@kevia.me>
2020-12-17 07:24:48 -0700

    Refactor cotter to instantiate only once

--- src/auth/login.js
+++ src/auth/login.js
@@ -2,19 +2,20 @@ import * as React from 'react'
 import Cotter from 'cotter'
 import styled from 'styled-components'
 
+const cotter = new Cotter({
+    ApiKeyID: process.env.GATSBY_COTTER_API,
+    Styles: {
+        input_label: {
+            color: '#f8f8f2',
+            fontSize: 16,
+        },
+    },
+})
+
 export default function Login(props) {
     const [payload, setPayload] = React.useState()
     const loginFormSetup = async () => {
         try {
-            const cotter = new Cotter({
-                ApiKeyID: process.env.GATSBY_COTTER_API,
-                Styles: {
-                    input_label: {
-                        color: '#f8f8f2',
-                        fontSize: 16,
-                    },
-                },
-            })
             const response = await cotter.signInWithLink().showEmailForm()
             setPayload(response)
         } catch (err) {
@@ -26,9 +27,20 @@ export default function Login(props) {
         loginFormSetup()
     }, [])
     return (
-        <Form id="cotter-form-container">
-            <pre>{JSON.stringify(payload, null, 4)}</pre>
-        </Form>
+        <div>
+            <Form id="cotter-form-container">
+                <pre>{JSON.stringify(payload, null, 4)}</pre>
+            </Form>
+            <button
+                onClick={() => {
+                    cotter.tokenHandler.getAccessToken().then(tok => {
+                        console.log(tok)
+                    })
+                }}
+            >
+                Check Token
+            </button>
+        </div>
     )
 }
 

5603cb894565771962dcd96373ccc2bb88fc654b
jhamPac <cloud@kevia.me>
2020-12-17 07:49:34 -0700

    Refactor landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,45 @@
 import * as React from 'react'
-import Home from '../home/home'
+import styled from 'styled-components'
 
 export default function Main() {
     return (
-        <main id="main-wrapper">
-            <Home />
+        <main id="landing-page">
+            <HeroSection>
+                <div>
+                    <h1>
+                        The <span style={{ color: '#f1fa8c' }}>FRIENDLY</span> software
+                        engineering Q & A website for the 21st century
+                    </h1>
+                </div>
+                <HeroCopy>
+                    <div>
+                        <p>
+                            Ask a question using screen recordings, code and connect live
+                            with video chat all on one platform.
+                        </p>
+                    </div>
+                    <div className="button-group">
+                        <button>Ask a question</button>
+                        <button>View top questions</button>
+                    </div>
+                </HeroCopy>
+            </HeroSection>
         </main>
     )
 }
+
+const HeroSection = styled.div`
+    margin-top: 32px;
+`
+
+const HeroCopy = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    .button-group {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+`

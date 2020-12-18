550d04c1166fa21092a9a79d941fca5970d2d886
jhamPac <cloud@kevia.me>
2020-12-17 22:09:34 -0700

    Move landing page to its own component

---
+++ src/landing/home.js
@@ -0,0 +1,98 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import { navigate } from 'gatsby'
+
+export default function Home() {
+    return (
+        <main id="landing-page">
+            <HeroSection>
+                <div>
+                    <h1>
+                        The <span style={{ color: '#f1fa8c' }}>FRIENDLY</span> software
+                        engineering Q & A website for the 21st century
+                    </h1>
+                </div>
+                <HeroCopy>
+                    <div className="copy">
+                        <p style={{ fontSize: '18px' }}>
+                            Ask a question using screen recordings, code and connect live
+                            with video chat all on one platform.
+                        </p>
+                    </div>
+                    <div className="button-group">
+                        <button onClick={() => navigate('/app/p/view')}>
+                            Ask a question
+                        </button>
+                        <button onClick={() => navigate('/app/top-questions')}>
+                            View top questions
+                        </button>
+                    </div>
+                </HeroCopy>
+            </HeroSection>
+            <FeatureSection>
+                <div className="feature-box"></div>
+                <div className="feature-box"></div>
+                <div className="feature-box"></div>
+            </FeatureSection>
+        </main>
+    )
+}
+
+const HeroSection = styled.div`
+    margin-top: 32px;
+    margin-bottom: 64px;
+`
+
+const HeroCopy = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    margin-top: 64px;
+
+    @media screen and (min-width: 769px) {
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    .copy {
+        @media screen and (min-width: 769px) {
+            width: 60%;
+        }
+    }
+
+    .button-group {
+        button:first-child {
+            margin-right: 8px;
+
+            @media screen and (min-width: 769px) {
+                margin-bottom: 8px;
+            }
+        }
+    }
+`
+
+const FeatureSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    @media screen and (min-width: 769px) {
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    .feature-box {
+        background: #6272a4;
+        width: 250px;
+        height: 250px;
+
+        margin-bottom: 32px;
+
+        @media screen and (min-width: 769px) {
+            margin-right: 32px;
+        }
+    }
+`
+
+const QuoteSection = styled.div``

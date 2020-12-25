b25c7992966e1057192cd86e202f317ec434b5ca
jhamPac <cloud@kevia.me>
2020-12-24 17:35:37 -0700

    Create page for users already logged in

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -4,8 +4,39 @@ import styled from 'styled-components'
 import { navigate } from 'gatsby'
 
 import { useAuth } from '@hooks/auth'
+const cotterToken = require('cotter-token-js')
 
 export default function Login(props) {
+    const { isLoggedIn } = useAuth()
+
+    return isLoggedIn ? <AlreadyLoggedIn {...props} /> : <ContinueLogin {...props} />
+}
+
+function AlreadyLoggedIn() {
+    let userEmail
+    if (window !== undefined) {
+        const u = window.localStorage.getItem('a_tk')
+
+        if (u === null) {
+            userEmail = 'unknown'
+            return
+        }
+
+        const data = new cotterToken.CotterAccessToken(u)
+        userEmail = data.payload.identifier
+    }
+
+    return (
+        <ContentWrapper>
+            <div className="logged-in">
+                <h3>{`You are already logged in as: ${userEmail}`}</h3>
+                <p>asdfasd</p>
+            </div>
+        </ContentWrapper>
+    )
+}
+
+function ContinueLogin(props) {
     const { saveToken } = useAuth()
     const [error, setError] = React.useState()
 
@@ -25,7 +56,7 @@ export default function Login(props) {
             setTimeout(() => {
                 const redirectURL = props.location.state.prevURL ?? '/'
                 navigate(redirectURL)
-            }, 1200)
+            }, 1500)
         } catch (err) {
             setError(true)
         }
@@ -34,29 +65,43 @@ export default function Login(props) {
     React.useEffect(() => {
         loginFormSetup()
     }, [])
+
+    // const t = window.localStorage.getItem('a_tk')
+    // console.log(t)
+    // console.log(new cotterToken.CotterAccessToken(t))
+
     return (
-        <FormWrapper>
+        <ContentWrapper>
             <Form id="cotter-form-container">
                 <h3>We use magic links for account creation and logging in.</h3>
             </Form>
             {error ? (
                 <p>An error has occured while attempting to login in, please try again</p>
             ) : null}
-        </FormWrapper>
+        </ContentWrapper>
     )
 }
 
-const FormWrapper = styled.div`
+const ContentWrapper = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: center;
 
     margin-top: 64px;
+
+    .logged-in {
+        width: 100%;
+        height: 500px;
+
+        @media screen and (min-width: 769px) {
+            width: 75%;
+        }
+    }
 `
 
 const Form = styled.div`
     width: 300px;
-    height: 300px;
+    height: 500px;
 
     h3 {
         margin-bottom: 0;

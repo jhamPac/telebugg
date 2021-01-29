627d6958eedc452759f3c0a87acea0561b9ff1c0
jhamPac <cloud@kevia.me>
2021-01-28 19:35:15 -0700

    Complete jest test for the footer

--- src/components/layout/spec/footer.spec.js
+++ src/components/layout/spec/footer.spec.js
@@ -1,9 +1,12 @@
 import * as React from 'react'
+import { render, screen } from '@testing-library/react'
 
 import Footer from '../footer'
 
 describe('Footer component', () => {
-    it('should pass', () => {
-        expect(true).toBe(true)
+    it('should render with a 4 digit year in the copyright', () => {
+        render(<Footer></Footer>)
+        const copyright = screen.getByText(/^[0-9]{4}\stelebugg$/i)
+        expect(copyright).toBeInTheDocument()
     })
 })

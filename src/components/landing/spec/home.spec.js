a72b79a22ac1a793d0df3b76922dd48fe50f613f
jhamPac <cloud@kevia.me>
2021-01-29 09:51:11 -0700

    Add userevent dep

--- src/components/landing/spec/home.spec.js
+++ src/components/landing/spec/home.spec.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import Home from '@components/landing/home'
 import { render, screen } from '@testing-library/react'
+import { navigate } from 'gatsby'
+
+// mocks
+jest.mock('gatsby')
 
 describe('Landing page Home component', () => {
     it('should render', () => {
         render(<Home />)
-
+        const button = screen.getByRole('button', { name: 'Ask a question' })
+        console.log(button)
         expect(true).toBe(true)
     })
 })

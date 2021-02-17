9cc2388c6701cb8d62be9413a7ec9f0ca9d95090
jhamPac <cloud@kevia.me>
2021-02-16 22:47:03 -0700

    refactor: wow, need to update aliases in 3 different places: gatsby-config, tsconfig, jest

--- src/app/home/spec/home.spec.js
+++ src/app/home/spec/home.spec.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import Home from '@components/home'
+import Home from '@app/home'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { navigate } from 'gatsby'

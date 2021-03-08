317dc29abcd7fba919547e3b1e900ea96decee2a
jhamPac <cloud@kevia.me>
2021-03-07 19:56:37 -0700

    test: made candidate row eaiser to test with dep injection

--- src/app/voting/spec/candidate-row.spec.js
+++ src/app/voting/spec/candidate-row.spec.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import CandidateRow from '../candidate-row'
 
 // mocks
@@ -8,20 +9,26 @@ const mockContract = {
     voteFor: name => Promise.resolve(true),
 }
 
+function testRender() {
+    render(<CandidateRow contract={mockContract} name="alice" />)
+}
+
 describe('CandidateRow component', () => {
     test('candidate should have 1 vote', async () => {
-        render(<CandidateRow contract={mockContract} name="alice" />)
+        testRender()
         const voteText = await waitFor(() => screen.getByText(/alice has 1 votes/i))
         expect(voteText).toBeInTheDocument()
     })
 
     test('up vote should have been pressed', async () => {
         const spy = jest.spyOn(mockContract, 'voteFor')
-        render(<CandidateRow contract={mockContract} name="alice" />)
+        testRender()
         const voteButton = await waitFor(() =>
             screen.getByRole('button', { name: /vote/i })
         )
 
-        expect(true).toBe(true)
+        await waitFor(() => userEvent.click(voteButton))
+
+        expect(spy).toHaveBeenCalled()
     })
 })

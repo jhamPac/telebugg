71383dd272bb89a160b0e0abf3fecf3250b0b0f8
jhamPac <cloud@kevia.me>
2021-04-11 15:07:34 -0700

    chore: mock the top question feed and the view for single questions

--- src/app/login/index.tsx
+++ src/app/login/index.tsx
@@ -2,5 +2,5 @@ import * as React from 'react'
 import { RouteComponentProps } from '@reach/router'
 
 export default function Login(props: RouteComponentProps) {
-    return <div>Wallet login</div>
+    return <div>coming soon...</div>
 }

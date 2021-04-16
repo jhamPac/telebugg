c508c94a68ab348736f207c4ede809874937c897
jhamPac <cloud@kevia.me>
2021-04-15 21:49:54 -0700

    fix: remove cotter import

--- src/app/account/dashboard.tsx
+++ src/app/account/dashboard.tsx
@@ -1,16 +1,5 @@
 import * as React from 'react'
-import { CotterAccessToken } from 'cotter-token-js'
-import useLocalStorage from '@hooks/useLocalStorage'
 
 export default function Account(): React.ReactElement | null {
-    const [token] = useLocalStorage('a_tk')
-
-    if (token === null) {
-        return null
-    }
-
-    const data = new CotterAccessToken(token)
-    const userEmail = data.payload.identifier
-
-    return <h3>{`Welcome ${userEmail}`}</h3>
+    return <div>under construction</div>
 }

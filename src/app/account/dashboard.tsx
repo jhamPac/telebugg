2bec27051d9a738f949e2244b752355dad3955a9
jhamPac <cloud@kevia.me>
2021-04-17 19:49:28 -0700

    refactor: move ipfs code over to screen recording section

--- src/app/account/dashboard.tsx
+++ src/app/account/dashboard.tsx
@@ -1,18 +1,6 @@
 import * as React from 'react'
 import { RouteComponentProps } from '@reach/router'
-import ipfs from 'ipfs-http-client'
 
 export default function Account(props: RouteComponentProps): React.ReactElement {
-    const client = ipfs()
-
-    const getVersion = async () => {
-        const v = await client.version()
-        console.log('BAM')
-    }
-
-    React.useEffect(() => {
-        getVersion()
-    })
-
     return <div>under construction</div>
 }
